2a01016c81bfcc7e6932f337d3f6719f11e6e76f
Michael Maliniak <mike@maliniak.io>
2019-08-30 14:24:53 +0800

    modified routes message

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Surf-stat App', message: 'Hello froggy!'});
+    res.render('index', { title: 'Surf-stat App', message: 'Surf\'s up!'});
 });
 
 module.exports = router;
